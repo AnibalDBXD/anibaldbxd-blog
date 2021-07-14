bdb290066c1d017a6fb78a794b78c99c2a287cff
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-14 17:12:34 -0400

    add filter to postList

--- components/PostList/index.tsx
+++ components/PostList/index.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable quote-props */
 import {
   RadioGroup, HStack, Radio, VStack,
 } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IPost } from './types';
 import Post from './Post';
+import { filterPostsByTag, ANIME_TAG, MANGA_TAG } from './utils';
 
 interface IProps {
   posts: IPost[];
@@ -11,6 +13,21 @@ interface IProps {
 
 function PostList({ posts }: IProps): JSX.Element {
   const [radio, setRadio] = useState('1');
+  const [currentPosts, setCurrentPosts] = useState(posts);
+
+  useEffect(() => {
+    const POSTS_OPTIONS = {
+      // all posts
+      '1': posts,
+      // anime posts
+      '2': filterPostsByTag(posts, ANIME_TAG),
+      // manga posts
+      '3': filterPostsByTag(posts, MANGA_TAG),
+    };
+
+    setCurrentPosts(POSTS_OPTIONS[radio] || posts);
+  }, [radio, posts]);
+
   return (
     <>
       <RadioGroup marginBottom="3rem" onChange={setRadio} value={radio}>
@@ -21,7 +38,7 @@ function PostList({ posts }: IProps): JSX.Element {
         </HStack>
       </RadioGroup>
       <VStack alignItems="baseline" marginTop="1rem" spacing="32px">
-        {posts.map((post) => (
+        {currentPosts.map((post) => (
           <Post key={post.id} {...post} />
         ))}
       </VStack>
---
+++ components/PostList/utils.ts
@@ -0,0 +1,13 @@
+import tagsToString from '../../utils/tagsToString';
+import { IPost } from './types';
+
+export const ANIME_TAG = 'anime';
+export const MANGA_TAG = 'manga';
+
+export function filterPostsByTag(posts: IPost[], tag: string): IPost[] {
+  const filteredPosts = posts.filter(({ properties: { Tags } }) => {
+    const postTag = tagsToString(Tags);
+    return postTag.includes(tag);
+  });
+  return filteredPosts;
+}
