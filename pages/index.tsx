62f926ddafa2581a51f9b8e53f24b20564e11187
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-15 18:10:04 -0400

    in home add more button to push to /more

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,34 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Button, useColorModeValue } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 import { GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 import PostList from '../components/PostList';
 import { getDatabase } from '../lib/notion';
 import TextWithLine from '../components/TextWithLine';
+import { primaryColor } from '../chakra/colors';
+import PATHS from '../config/paths';
 
 export const databaseId = process.env.NOTION_DATABASE_ID;
 
 export default function Home({ posts }): JSX.Element {
+  const buttonColor = useColorModeValue(primaryColor.light, primaryColor.dark);
+  const { push } = useRouter();
   return (
     <Box paddingX="20px">
       <TextWithLine>
         Publicaciones
       </TextWithLine>
       <PostList posts={posts} />
+      <Button
+        borderColor={buttonColor}
+        color={buttonColor}
+        marginTop="2rem"
+        onClick={(): void => { push(PATHS.more); }}
+        rightIcon={<ExternalLinkIcon />}
+        variant="outline"
+      >
+        More
+      </Button>
     </Box>
   );
 }
