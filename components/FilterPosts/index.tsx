6ef4825dabff2b4c44590fd30719665921884acf
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-15 16:21:33 -0400

    Create component <FilterPosts />

---
+++ components/FilterPosts/index.tsx
@@ -0,0 +1,38 @@
+import { HStack, Radio, RadioGroup } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { IPost } from '../../interfaces/types';
+import { filterPostsByTag, ANIME_TAG, MANGA_TAG } from './utils';
+
+interface IFilterPosts {
+    setPosts: (posts: IPost[]) => void;
+    posts: IPost[];
+}
+
+const FilterPosts = ({ setPosts, posts }: IFilterPosts): JSX.Element => {
+  const [radio, setRadio] = useState('1');
+
+  useEffect(() => {
+    const POSTS_OPTIONS = {
+      // all posts
+      1: posts,
+      // anime posts
+      2: filterPostsByTag(posts, ANIME_TAG),
+      // manga posts
+      3: filterPostsByTag(posts, MANGA_TAG),
+    };
+
+    setPosts(POSTS_OPTIONS[radio] || posts);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [radio]);
+  return (
+    <RadioGroup marginBottom="3rem" onChange={setRadio} value={radio}>
+      <HStack spacing="20px">
+        <Radio value="1">Todos</Radio>
+        <Radio value="2">Anime</Radio>
+        <Radio value="3">Manga</Radio>
+      </HStack>
+    </RadioGroup>
+  );
+};
+
+export default FilterPosts;
--- components/PostList/index.tsx
+++ components/PostList/index.tsx
@@ -1,42 +1,24 @@
 /* eslint-disable quote-props */
 import {
-  RadioGroup, HStack, Radio, VStack,
+  VStack,
 } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { IPost } from '../../interfaces/types';
+import FilterPosts from '../FilterPosts';
 import Post from '../Post';
-import { filterPostsByTag, ANIME_TAG, MANGA_TAG } from './utils';
 
 interface IProps {
   posts: IPost[];
 }
 
 function PostList({ posts }: IProps): JSX.Element {
-  const [radio, setRadio] = useState('1');
   const [currentPosts, setCurrentPosts] = useState(posts);
 
-  useEffect(() => {
-    const POSTS_OPTIONS = {
-      // all posts
-      '1': posts,
-      // anime posts
-      '2': filterPostsByTag(posts, ANIME_TAG),
-      // manga posts
-      '3': filterPostsByTag(posts, MANGA_TAG),
-    };
-
-    setCurrentPosts(POSTS_OPTIONS[radio] || posts);
-  }, [radio, posts]);
+  const setPosts = (newPosts: IPost[]): void => setCurrentPosts(newPosts);
 
   return (
     <>
-      <RadioGroup marginBottom="3rem" onChange={setRadio} value={radio}>
-        <HStack spacing="20px">
-          <Radio value="1">Todos</Radio>
-          <Radio value="2">Anime</Radio>
-          <Radio value="3">Manga</Radio>
-        </HStack>
-      </RadioGroup>
+      <FilterPosts posts={posts} setPosts={setPosts} />
       <VStack alignItems="baseline" marginTop="1rem" spacing="32px">
         {currentPosts.map((post) => (
           <Post key={post.id} {...post} />
