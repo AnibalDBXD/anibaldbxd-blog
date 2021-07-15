746ad2d73f6b593244e85f2a48f442f181cd7ed5
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-15 18:22:49 -0400

    Add <Head /> in more page

--- pages/more/index.tsx
+++ pages/more/index.tsx
@@ -1,19 +1,29 @@
 import { Box } from '@chakra-ui/react';
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 import TextWithLine from '../../components/TextWithLine';
 import PostGrid from '../../components/PostGrid';
 import { getDatabase } from '../../lib/notion';
+import user from '../../config/user';
 
 const databaseId = process.env.NOTION_MORE_DATABASE;
 
 export default function More({ posts }): JSX.Element {
   return (
-    <Box paddingX="20px">
-      <TextWithLine>
-        Mas publicaciones
-      </TextWithLine>
-      <PostGrid haveContent={false} posts={posts} />
-    </Box>
+    <>
+      <Head>
+        <title>
+          More
+          {user.pageTitle}
+        </title>
+      </Head>
+      <Box paddingX="20px">
+        <TextWithLine>
+          Mas publicaciones
+        </TextWithLine>
+        <PostGrid haveContent={false} posts={posts} />
+      </Box>
+    </>
   );
 }
 
