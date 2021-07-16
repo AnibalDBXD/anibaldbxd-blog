cde9fac80aaca915093b9aa70bd83d3dcc86b798
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-16 16:49:56 -0400

    fix next-18next config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,12 +1,19 @@
 const path = require('path');
 
-// es: Spanish
-// en: English
+const SPANISH = 'es';
+const ENGLISH = 'en';
+
+const SUPPORT_LANGUAGE = [SPANISH, ENGLISH];
 
 module.exports = {
   i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
+    defaultLocale: SPANISH,
+    locales: SUPPORT_LANGUAGE,
     localePath: path.resolve('./config/locales'),
+  },
+  languages: {
+    SPANISH,
+    ENGLISH,
+    SUPPORT_LANGUAGE,
   },
 };
