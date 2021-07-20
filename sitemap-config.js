ca8cdfa05b8436acc73d407d00de42aac79411e6
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-20 14:35:35 -0400

    add next-sitemap

---
+++ sitemap-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteUrl: 'https://anibaldbxd.cf',
+  generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://anibaldbxd.cf/server-sitemap.xml',
+    ],
+  },
+};
