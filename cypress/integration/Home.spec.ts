8fb2658a438359a797f3a2ecf33acb1506cc362f
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-31 18:47:53 -0400

    update Home tests

--- cypress/integration/Home.spec.ts
+++ cypress/integration/Home.spec.ts
@@ -1,8 +1,25 @@
 import user from '../../config/user';
+import PATHS from '../../config/paths';
+import { defaultLang } from '../fixtures/language';
 
 describe('Home page', () => {
+  beforeEach(() => {
+    cy.visit(PATHS.home);
+  });
   it('Home page can be opened', () => {
-    cy.visit('/');
-    cy.contains(user.title.toLocaleString());
+    // Check page content
+    cy.findByText(user.title.toLocaleString());
+
+    cy.findByText(defaultLang.description);
+  });
+
+  it('In home can visit /more page', () => {
+    cy.findByText(defaultLang.more).click();
+    cy.url().should('contain', PATHS.more);
+  });
+
+  it('In home can visit the first post', () => {
+    cy.findAllByRole('list').findAllByRole('link').first().click();
+    cy.url().should('not.equal', `http://localhost:3000${PATHS.home}`);
   });
 });
