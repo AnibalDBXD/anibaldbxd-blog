4be933f23a160aaba04ec0ee42e1045f421747b7
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-07-31 22:21:16 -0400

    add translates ThemeToggler arial label

--- components/ThemeToggler/index.tsx
+++ components/ThemeToggler/index.tsx
@@ -1,11 +1,13 @@
 import { IconButton, useColorMode } from '@chakra-ui/react';
 import { SunIcon, MoonIcon } from '@chakra-ui/icons';
+import { useTranslation } from 'next-i18next';
 
 function ThemeToggler(): JSX.Element {
   const { colorMode, toggleColorMode } = useColorMode();
+  const { t } = useTranslation('common');
   return (
     <IconButton
-      aria-label={`Change ${colorMode} mode`}
+      aria-label={t('toggleThemeLabel')}
       border="0"
       color={`${colorMode === 'dark' ? 'white' : 'black'}`}
       icon={colorMode === 'light' ? <SunIcon /> : <MoonIcon />}
