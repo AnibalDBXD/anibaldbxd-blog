6cd6dad89d321581ada0319eeca28265a422761f
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-08-19 20:41:58 -0400

    Delete IconBack in header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,9 +1,7 @@
 import {
-  Heading, Link as ChakraLink, Flex, Text, useColorModeValue, IconButton,
+  Heading, Link as ChakraLink, Flex, Text, useColorModeValue,
 } from '@chakra-ui/react';
-import { ArrowBackIcon } from '@chakra-ui/icons';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
 import { heading, headingHover } from '../../lib/chakra/colors';
 
@@ -14,57 +12,34 @@ import ThemeToggler from '../ThemeToggler';
 interface IProps {
   title: ReactNode;
   description?: ReactNode;
-  isHome: boolean;
 }
 
-function Header({ title, description, isHome }: IProps): JSX.Element {
+function Header({ title, description }: IProps): JSX.Element {
   const headingColor = useColorModeValue(heading.light, heading.dark);
   const headingHoverColor = useColorModeValue(headingHover.light, headingHover.dark);
-  const { push } = useRouter();
   return (
     <Flex
       alignItems="center"
       as="header"
       flexWrap="wrap"
       justifyContent="space-between"
+      margin="0"
     >
-      {
-        isHome
-          ? (
-            <Heading>
-              <Link href={PATHS.home} passHref>
-                <ChakraLink
-                  _hover={{
-                    color: headingHoverColor,
-                    textDecoration: 'none',
-                  }}
-                  color={headingColor}
-                  fontSize="32px"
-                  fontWeight="500"
-                >
-                  {title}
-                </ChakraLink>
-              </Link>
-            </Heading>
-          )
-          : (
-            // Use IconButton instead of Link/ChakraLink because Link only render text, not icons
-            <IconButton
-              _hover={{
-                color: headingHoverColor,
-              }}
-              aria-label="go back"
-              backgroundColor="transparent"
-              border="0"
-              color={headingColor}
-              cursor="pointer"
-              icon={<ArrowBackIcon fontSize="46px" />}
-              onClick={(): void => { push(PATHS.home); }}
-              variant="link"
-            />
-          )
-      }
-
+      <Heading>
+        <Link href={PATHS.home} passHref>
+          <ChakraLink
+            _hover={{
+              color: headingHoverColor,
+              textDecoration: 'none',
+            }}
+            color={headingColor}
+            fontSize="32px"
+            fontWeight="500"
+          >
+            {title}
+          </ChakraLink>
+        </Link>
+      </Heading>
       <ThemeToggler />
       {description && <Text fontSize="lg" marginTop="1rem" width="100%">{description}</Text>}
     </Flex>
